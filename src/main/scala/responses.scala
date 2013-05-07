390735d33ec9d1d741658f0f9130a7fa4a784500
Eugene Yokota <eed3si9n@gmail.com>
2013-05-07 03:35:03 -0400

    adds User

--- src/main/scala/responses.scala
+++ src/main/scala/responses.scala
@@ -26,7 +26,7 @@ case class Tweet(
   id: BigInt,
   text: String,
   created_at: Calendar,
-  user: JObject,
+  user: Option[User],
   favorite_count: Option[Int],
   favorited: Option[Boolean],
   retweet_count: Int,
@@ -42,23 +42,18 @@ case class Tweet(
 
 /** https://dev.twitter.com/docs/platform-objects/tweets 
  */
-object Tweet extends Parse {
+object Tweet extends Parse with CommonField {
   val contributors   = 'contributors[List[JValue]]
   val coordinates    = 'coordinates[JObject]
-  val created_at     = 'created_at.![Calendar]
   val current_user_retweet = 'current_user_retweet[JObject]
-  val entities       = 'entities.![JObject]
   val favorite_count = 'favorite_count[Int]
   val favorited      = 'favorited[Boolean]
   val filtere_level  = 'filtere_level[String]
-  val id             = 'id.![BigInt]
-  val id_str         = 'id_str.![String]
   val in_reply_to_screen_name   = 'in_reply_to_screen_name[String]
   val in_reply_to_status_id     = 'in_reply_to_status_id[BigInt]
   val in_reply_to_status_id_str = 'in_reply_to_status_id_str[String]
   val in_reply_to_user_id       = 'in_reply_to_user_id[BigInt]
   val in_reply_to_user_id_str   = 'in_reply_to_user_id_str[String]
-  val lang           = 'lang[String]
   val place          = 'place[JObject]
   val possibly_sensitive = 'possibly_sensitive[Boolean]
   val scopes         = 'scopes[JObject]
@@ -67,16 +62,13 @@ object Tweet extends Parse {
   val retweeted      = 'retweeted.![Boolean]
   val text           = 'text.![String]
   val truncated      = 'truncated.![Boolean]
-  val user           = 'user.![JObject]
-  val withheld_copyright    = 'withheld_copyright[Boolean]
-  val withheld_in_countries = 'withheld_in_countries[List[JValue]]
-  val withheld_scope        = 'withheld_scope[String]
-
+  val user           = 'user[JObject]
+  
   def apply(js: JValue): Tweet = Tweet(
     id = id(js),
     text = text(js),
     created_at = created_at(js),
-    user = user(js),
+    user = user(js) map { case x => User(x) },
     favorite_count = favorite_count(js),
     favorited = favorited(js),
     retweet_count = retweet_count(js),
@@ -89,6 +81,103 @@ object Tweet extends Parse {
     in_reply_to_status_id = in_reply_to_status_id(js),
     in_reply_to_user_id = in_reply_to_user_id(js)   
   )
+}
+
+case class User(
+  id: BigInt,
+  screen_name: String,
+  created_at: Calendar,
+  name: String,
+  `protected`: Boolean,
+  description: Option[String],
+  location: Option[String],
+  time_zone: Option[String],
+  url: Option[String],
+  verified: Boolean,
+  statuses_count: Int,
+  favourites_count: Int,
+  followers_count: Int,
+  friends_count: Int,
+  default_profile: Boolean,
+  default_profile_image: Boolean,
+  profile_image_url: String,
+  profile_image_url_https: String,
+  lang: Option[String],
+  entities: JObject
+)
+
+/** https://dev.twitter.com/docs/platform-objects/users
+ */
+object User extends Parse with CommonField {
+  val contributors_enabled  = 'contributors_enabled.![Boolean]
+  val default_profile       = 'default_profile.![Boolean]
+  val default_profile_image = 'default_profile_image.![Boolean]
+  val description           = 'description[String]
+  val favourites_count      = 'favourites_count.![Int]
+  val follow_request_sent   = 'follow_request_sent[Boolean]
+  val followers_count       = 'followers_count.![Int]
+  val friends_count         = 'friends_count.![Int]
+  val geo_enabled           = 'geo_enabled.![Boolean]
+  val is_translator         = 'is_translator.![Boolean]
+  val listed_count          = 'listed_count.![Int]
+  val location              = 'location[String]
+  val name                  = 'name.![String]
+  val notifications         = 'notifications[Boolean]
+  val profile_background_color = 'profile_background_color.![String]
+  val profile_background_image_url = 'profile_background_image_url.![String]
+  val profile_background_image_url_https = 'profile_background_image_url_https.![String]
+  val profile_background_tile  = 'profile_background_tile.![Boolean]
+  val profile_banner_url    = 'profile_banner_url.![String]
+  val profile_image_url     = 'profile_image_url.![String]
+  val profile_image_url_https = 'profile_image_url_https.![String]
+  val profile_link_color    = 'profile_link_color.![String]
+  val profile_sidebar_border_color = 'profile_sidebar_border_color.![String]
+  val profile_sidebar_fill_color   = 'profile_sidebar_fill_color.![String]
+  val profile_text_color    = 'profile_text_color.![String]
+  val profile_use_background_image = 'profile_use_background_image.![Boolean]
+  val `protected`           = 'protected.![Boolean]
+  val screen_name           = 'screen_name.![String]
+  val show_all_inline_media = 'show_all_inline_media.![Boolean]
+  val status                = 'status[JObject]
+  val statuses_count        = 'statuses_count.![Int]
+  val time_zone             = 'time_zone[String]
+  val url                   = 'url[String]
+  val utc_offset            = 'utc_offset[String]
+  val verified              = 'verified.![Boolean]
+
+  def apply(js: JValue): User = User(
+    id = id(js),
+    screen_name = screen_name(js),
+    created_at = created_at(js),
+    name = name(js),
+    `protected` = `protected`(js),
+    description = description(js),
+    location = location(js),
+    time_zone = time_zone(js),
+    url = url(js),
+    verified = verified(js),
+    statuses_count = statuses_count(js),
+    favourites_count = favourites_count(js),
+    followers_count = followers_count(js),
+    friends_count = friends_count(js),
+    default_profile = default_profile(js),
+    default_profile_image = default_profile_image(js),
+    profile_image_url = profile_image_url(js),
+    profile_image_url_https = profile_image_url_https(js),
+    lang = lang(js),
+    entities = entities(js)
+  )
+}
+
+trait CommonField { self: Parse =>
+  val id                    = 'id.![BigInt]
+  val id_str                = 'id_str.![String]
+  val created_at            = 'created_at.![Calendar]
+  val entities              = 'entities.![JObject]
+  val lang                  = 'lang[String]
+  val withheld_copyright    = 'withheld_copyright[Boolean]
+  val withheld_in_countries = 'withheld_in_countries[List[JValue]]
+  val withheld_scope        = 'withheld_scope[String]
 }
 
 trait Parse {
