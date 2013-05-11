45440d7d09cbf5474022737b0b1475c51610d44b
Eugene Yokota <eed3si9n@gmail.com>
2013-05-11 18:30:33 -0400

    sonatype settings

---
+++ README.md
@@ -0,0 +1,4 @@
+repatch-twitter
+===============
+
+repatch-twitter is a Dispatch 0.10 plugin for Twitter API.
--- project/build.scala
+++ project/build.scala
@@ -6,7 +6,7 @@ object Builds extends Build {
 
   lazy val buildSettings = Defaults.defaultSettings ++ Seq(
     dispatchVersion := "0.10.0",
-    version <<= dispatchVersion { dv => dv + "_0.1.0-SNAPSHOT" },
+    version <<= dispatchVersion { dv => "dispatch" + dv + "_0.1.0-SNAPSHOT" },
     organization := "com.eed3si9n",
     scalaVersion := "2.10.1",
     libraryDependencies <++= (dispatchVersion) { (dv) => Seq(
@@ -17,8 +17,34 @@ object Builds extends Build {
       case "2.9.3" =>  "org.specs2" %% "specs2" % "1.12.4.1" % "test"
       case _ => "org.specs2" %% "specs2" % "1.15-SNAPSHOT" % "test"
     },
-    crossScalaVersions := Seq("2.10.1"),
-    resolvers += "sonatype-public" at "https://oss.sonatype.org/content/repositories/public"
+    crossScalaVersions := Seq("2.10.1"),    
+    resolvers += "sonatype-public" at "https://oss.sonatype.org/content/repositories/public",
+    licenses := Seq("MIT License" -> url("http://opensource.org/licenses/MIT")),
+    description := """repatch-twitter is a Dispatch plugin for Twitter API.""",
+    pomExtra := (<scm>
+        <url>git@github.com:eed3si9n/repatch-twitter.git</url>
+        <connection>scm:git:git@github.com:eed3si9n/repatch-twitter.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>eed3si9n</id>
+          <name>Eugene Yokota</name>
+          <url>http://eed3si9n.com</url>
+        </developer>
+      </developers>),
+    publishArtifact in Test := false,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
+      else Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    publishMavenStyle := true,
+    pomIncludeRepository := { x => false },
+    parallelExecution in Test := false,
+    crossVersion <<= scalaVersion { sv =>
+      ("-(M|RC)".r findFirstIn sv) map {_ => CrossVersion.full} getOrElse CrossVersion.binary
+    }
   )
   lazy val coreSettings = buildSettings ++ Seq(
     name := "repatch-twitter-core"
